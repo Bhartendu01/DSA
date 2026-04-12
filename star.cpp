76895ad0852ba0aca99fcf359dd294d68979c215
Bhartendu01 <saurabhbhartendu23@gmail.com>
2026-04-12 18:43:32 +0530

    star printing question

---
+++ star.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// task hai star print karwana 
+
+int main(){
+    int n; 
+    cout<<"Enter the mala ka size: ";
+    cin>>n;
+    // if(n==1){
+    //     cout<<"*";
+    // }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==0||i==n-1 ||j==0||j==n-1){
+                cout<<"* ";
+            }else{
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
